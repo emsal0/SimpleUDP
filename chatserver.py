d6decf41d754dd10145b6af24664e5d2eb987428
ejs <absolutezero2a03@gmail.com>
2012-12-24 21:22:24 -0500

    Changed chatserver.py so that it runs from a function.

--- chatserver.py
+++ chatserver.py
@@ -2,22 +2,25 @@
 
 import socket, random
 
-serv=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-SERV = raw_input("your ip : ")
-serv.bind((SERV,6667))
 
-people={}
+def run():
+	serv=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+	SERV = raw_input("your ip : ")
+	serv.bind((SERV,6667))
 
-while 1:
-	data,addr=serv.recvfrom(65536)
+	people={}
+	while 1:
+		data,addr=serv.recvfrom(65536)
 
-	if addr[0] not in people.keys():
-		if data not in people.values():
-			people[addr[0]] = data
-		else:
-			people[addr[0]] = data + "_"
-		continue
-	print addr,": ",data
-	for i in people:
-		serv.sendto(people[addr[0]]+": "+data,(i,6668))
-serv.close()
+		if addr[0] not in people.keys():
+			if data not in people.values():
+				people[addr[0]] = data
+			else:
+				people[addr[0]] = data + "_"
+			continue
+		print addr,": ",data
+		for i in people:
+			serv.sendto(people[addr[0]]+": "+data,(i,6668))
+	serv.close()
+if __name__ == "__main__":
+	run()
